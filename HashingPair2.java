f0994b3ed4f14f4200ba8b9d7937b380af646586
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-07-10 19:46:46 +0530

    Create HashingPair2.java

---
+++ HashingPair2.java
@@ -0,0 +1,20 @@
+ public static int sumExists(int arr[], int N, int sum)
+    {
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int i=0;i<N;i++)
+        {
+            if(hs.contains(sum-arr[i]))
+            {
+                return 1;
+            }
+            else
+            {
+                hs.add(arr[i]);
+            }
+        }
+       
+        return 0;
+        
+        
+     }

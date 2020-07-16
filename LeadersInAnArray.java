7ade4a15555fbc2cad418331a3bec8c947b72535
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-07-16 15:37:37 +0530

    Create LeadersInAnArray.java
    Finding leaders in array in O(n) tc

---
+++ LeadersInAnArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        
+        int count = 0;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=0)
+            {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
+            }
+        }        
+    }
+}

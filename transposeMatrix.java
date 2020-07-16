2e94085415b5200839f7c9a4ebbfd72f8c4817d3
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-07-16 23:15:01 +0530

    Create transposeMatrix.java

---
+++ transposeMatrix.java
@@ -0,0 +1,13 @@
+ static void transpose(int a[][], int n)
+    {
+        // Your code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+        }
+    }

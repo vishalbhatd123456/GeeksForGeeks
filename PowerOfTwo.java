2763471601210f57452e0def0f655f32908ad013
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-07-16 11:25:31 +0530

    Create PowerOfTwo.java

---
+++ PowerOfTwo.java
@@ -0,0 +1,9 @@
+public static boolean isPowerofTwo(long n){
+        
+        // Your code here
+        if(n == 0)
+        return false;
+        
+        return ((n&(n-1)) == 0);
+        
+    }
